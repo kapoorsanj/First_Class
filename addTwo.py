8b140be2f9f6415e5bde49fca22a532922067139
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-07-16 12:37:06 +0300

    modified the file addtwo.py

--- addTwo.py
+++ addTwo.py
@@ -1,8 +1,11 @@
 # A program to add two numbers
-
+# First Define a function
 def sum(a,b):
     sum=a+b
+    # Return the value of sum
     return sum
 
+# Enter the values to be added
 sum1=sum(50,30)
+# Finally print sum
 print(sum1)
