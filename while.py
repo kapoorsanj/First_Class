62c3b1f529f6f692bc4eaf9ef6f27249801cc53b
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-07-22 11:03:33 +0300

    Corrected errors in prog. while.py

--- while.py
+++ while.py
@@ -1,12 +1,25 @@
-answer=25
-q="what no. am I thinking?"
-print("lets play a game")
+print("A program to Troubleshoot Computer Power failure.\n")
+answer='Y'
+print("________________________\n")
+question="Is Power cord connected to power socket"
+print("Type Y for Yes, N for no\n")
+
 while True:
-    guess=int(input(q))
-    if(guess<answer):
-        print("A little higher")
-    elif(guess>answer):
-        print("A little lower")
+    g=(input(question))
+    if(g==answer):
+        print("Press the ON SWITCH, TYPE Y for POWERED ON or N for OFF\n")
+        question1="IS THE COMPUTER POWERED UP"
+        g1=input(question1)
+        if(g1==answer):
+            print("HURRAY THE COMPUTER SWITCHED ON")
+            print("_________________________________\n")
+            break;
+        elif(g1!=answer):
+            print("The COMPUTER NEEDS REPAIR")
+            print("__________________________\n")
+            break;
+
     else:
-       print("You got it!")
-       break;
+        print("Get COMPUTER REPAIRED")
+        print("__________________________\n")
+        break;         
