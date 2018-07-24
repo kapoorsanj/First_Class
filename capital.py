30abcb26f562e6ac4fd11ada62f31630879c2ec9
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-07-24 14:59:26 +0300

    created a function to capitalize capital.py

--- capital.py
+++ capital.py
@@ -1,3 +1,5 @@
-message=["the quick brown fox jumps over the lasy dog"]
-for text in message:
-    print(text.title())
+def message():
+    message=["the quick brown fox jumps over the lasy dog"]
+    for text in message:
+        print(text.title())
+message()
