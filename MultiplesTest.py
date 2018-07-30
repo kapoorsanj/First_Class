2b254347b16b0f37702cbc7b444549ceab17ddce
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-07-30 11:46:07 +0300

    added a multiples module pro multiples.py

---
+++ MultiplesTest.py
@@ -0,0 +1,14 @@
+import multiples
+
+print("\n_Multiples of Two______________________")
+multiples.Multiples_of_Two()
+
+print("\n_Multiples of Three______________________")
+multiples.Multiples_of_Three()
+
+print("\n_Multiples of Four______________________")
+multiples.Multiples_of_Four()
+
+print("\n_Multiples of Nine______________________")
+multiples.Multiples_of_Nine()
+
