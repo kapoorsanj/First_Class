8bf3a58078e83df96ff44270c7a8926039a719fc
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-07-31 12:09:41 +0300

    added isnan to packages

---
+++ isnan.py
@@ -0,0 +1,4 @@
+# A program that tells us wheather its a number or not.
+from math import *
+t=isnan(5)
+print("The isnan is:=",t)
--- packages.py
+++ packages.py
@@ -1,4 +1,6 @@
 # A program that deals with packages both external and user defined.
 from math import *
 fact=factorial(5)
-print("The factorial of number 5! is =:",fact)
+#print("The factorial of number 5! is =:",fact)
+t=isnan(fact)
+print("the isnan of factorial is:=", t)
