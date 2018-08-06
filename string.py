2bed3dd721649d70408a01039421146a79f49f96
Sanjiv Kapoor <kapoor.sanj@gmail.com>
2018-08-06 16:23:14 +0300

    homework exercise string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+a = 'John'
+b = 'Doe'
+c = 'Smith'
+
+SEP=" "
+d=b[0]
+FullName = a+SEP+d+SEP+c
+print("______________________________________")
+print("your full name is:\n",FullName         )
+print("______________________________________")
